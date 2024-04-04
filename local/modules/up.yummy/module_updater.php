dc0ac5ba9313ca06062091dd88e161809f59e9e8
Zmachinskii Dmitrii <zmachinskiiof@gmail.com>
2024-04-04 22:00:40 +0200

    Добавлены данные в БД

--- local/modules/up.yummy/module_updater.php
+++ local/modules/up.yummy/module_updater.php
@@ -26,11 +26,47 @@ function __yummyMigrate(int $nextVersion, callable $callback): void
 }
 
 
-/*__yummyMigrate(2, function ($updater, $DB) {
+__yummyMigrate(2, function ($updater, $DB) {
 	if ($updater->CanUpdateDatabase())
 	{
+		$DB->query("INSERT INTO categories(title)
+VALUES
+('Мясо'),
+('Рыба'),
+('Птица'),
+('Морепродукты'),
+('Овощи'),
+('Фрукты'),
+('Зелень'),
+('Грибы'),
+('Яйца/Яичные прдукты'),
+('Молоко/Молочные продукты'),
+('Соя/Соевые продукты'),
+('Жиры/Масла'),
+('Орехи'),
+('Крупы/Злаки'),
+('Семена'),
+('Мука/Мучные продукты'),
+('Специи/Пряности'),
+('Сладости'),
+('Напитки/Соки');");
 	}
-});*/
+});
+__yummyMigrate(3, function ($updater, $DB) {
+	if ($updater->CanUpdateDatabase())
+	{
+		$DB->query("INSERT INTO measures(title)
+VALUES
+('гр'),
+('кг'),
+('чн. лжк.'),
+('ст. лжк.'),
+('л'),
+('мл'),
+('шт'),
+('По вкусу');");
+	}
+});
 
 
 
