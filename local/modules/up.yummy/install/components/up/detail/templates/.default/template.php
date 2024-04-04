91048477dcb5c90bf8f6fe290c48b12911b74e5e
Mikhail Korol <mishakorol101@gmail.com>
2024-04-05 00:19:44 +0200

    Добавлены стили на деталку

--- local/modules/up.yummy/install/components/up/detail/templates/.default/template.php
+++ local/modules/up.yummy/install/components/up/detail/templates/.default/template.php
@@ -2,13 +2,40 @@
 
 ?>
 <div class="content">
-	<input class="input" type="text" placeholder="Text input" /><br>
-	<p>Название рецепта</p><br>
-	<p>Калории</p><br>
-	<p>Белки</p><br>
-	<p>Жиры</p><br>
-	<p>Углеводы</p><br>
-	<p>Время приготовления</p><br>
-	<p>Ингредиенты</p><br>
-	<p>Описание</p><br>
+	<input class="input" type="text" placeholder="Text input" />
+	<div class="columns">
+		<div class="column">
+		</div>
+		<div class="column">
+			<p class="title">Рецепт</p>
+			<div class="columns">
+				<div class="column">
+					<h5>Калории</h5>
+					<p>432423</p>
+				</div>
+				<div class="column">
+					<h5>Белки</h5>
+					<p>143232</p>
+				</div>
+				<div class="column">
+					<h5>Жиры</h5>
+					<p>32423</p>
+				</div>
+				<div class="column">
+					<h5>Углеводы</h5>
+					<p>534534</p>
+				</div>
+			</div>
+			<p><strong>Время приготовления:</strong> 45 мин</p>
+		</div>
+	</div>
+	<div class="container bottom_content">
+		<p class="title">Ингредиенты</p><br>
+
+		<div class="notification is-info">
+			<p class="title">Описание</p><br>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda dolore iste maiores repellendus sapiente sit? Consequatur esse ipsa ipsam laboriosam nesciunt quibusdam repudiandae. Consequuntur fugiat nobis pariatur vel. Accusamus aliquam atque, cupiditate ex facere illum incidunt iure libero molestias, nam, reiciendis veniam. A dolorem enim ex illo ipsa laudantium molestias, officiis provident quos rerum sed similique sit tempora temporibus vel. Asperiores aut consequuntur debitis ducimus enim, eveniet exercitationem illum laboriosam magnam molestiae natus, nobis nostrum numquam obcaecati quaerat quibusdam saepe sunt, tenetur! Ab ad aliquid cupiditate delectus deserunt dolor enim eum excepturi inventore laborum necessitatibus nostrum nulla officia optio placeat quod ratione reiciendis repellendus reprehenderit repudiandae sit, veniam, voluptatibus! Aliquam aliquid ducimus laboriosam necessitatibus nemo optio quidem repellendus similique sit temporibus. Blanditiis cum ea earum, fugiat fugit incidunt iure, minus non numquam odit pariatur placeat quis soluta! A aliquam aperiam aspernatur consectetur deleniti, deserunt ducimus id illum minus nemo neque nostrum placeat quod rerum sapiente tempora vero vitae! Adipisci alias, aut consectetur corporis culpa deleniti illo minima necessitatibus quibusdam similique veritatis vero voluptas. Aliquid ea libero provident! Accusantium aperiam assumenda consequuntur dolorem error facere harum, inventore ipsam itaque laboriosam magni maiores modi nam nulla optio quaerat quis, rerum sint velit voluptatibus? Aliquid aspernatur, blanditiis commodi corporis culpa eum explicabo, illo ipsam laudantium magni maiores officiis quis repellendus rerum saepe sed tenetur velit. Facere in nihil officiis, quos temporibus veniam? Commodi consectetur error illo labore obcaecati pariatur recusandae veritatis, voluptatibus. Ducimus eligendi harum ipsam ipsum iure magni nam quisquam sapiente!
+		</div>
+		</div>
+	</div>
 </div>
