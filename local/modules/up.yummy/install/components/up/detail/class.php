6632b9f8879d7a4c524e844d66401fa49b3abcd2
Mikhail Korol <mishakorol101@gmail.com>
2024-04-05 18:53:00 +0200

    Доделка деталки

--- local/modules/up.yummy/install/components/up/detail/class.php
+++ local/modules/up.yummy/install/components/up/detail/class.php
@@ -1,9 +1,16 @@
 <?php
 
-class TaskDocComponent extends CBitrixComponent
+class DetailComponent extends CBitrixComponent
 {
 	public function executeComponent()
 	{
+		$this->prepareTemplateParams();
 		$this->includeComponentTemplate();
+	}
+
+	protected function prepareTemplateParams(): void
+	{
+		$imagePath = $this->getPath() . '/images/default_image.jpg';
+		$this->arParams['IMAGE'] = $imagePath;
 	}
 }
--- local/modules/up.yummy/install/components/up/detail/templates/.default/template.php
+++ local/modules/up.yummy/install/components/up/detail/templates/.default/template.php
@@ -1,10 +1,16 @@
 <?php
-
+/**
+ * @var array $arParams;
+ */
 ?>
 <div class="content">
-	<input class="input" type="text" placeholder="Text input" />
+	<div class="column is-flex is-half is-offset-one-quarter">
+		<input class="input is-half" type="text" placeholder="Поиск рецепта" />
+		<button class="button is-light ml-3">Искать</button>
+	</div>
 	<div class="columns">
 		<div class="column">
+			<img src="<?=$arParams['IMAGE']?>"/>
 		</div>
 		<div class="column">
 			<p class="title">Рецепт</p>
@@ -27,10 +33,31 @@
 				</div>
 			</div>
 			<p><strong>Время приготовления:</strong> 45 мин</p>
+			<div class="buttons">
+				<button class="button is-success">Опубликовать рецепт</button>
+				<button class="button is-warning">Изменить рецепт</button>
+				<button class="button is-danger">Удалить рецепт</button>
+			</div>
 		</div>
 	</div>
 	<div class="container bottom_content">
 		<p class="title">Ингредиенты</p><br>
+		<div class="column is-half is-offset-one-quarter">
+			<table class="table">
+				<thead>
+				<tr>
+					<th>Продукт</th>
+					<th>Количество</th>
+				</tr>
+				</thead>
+				<tbody>
+				<tr>
+					<td>Морковь</td>
+					<td>300 гр</td>
+				</tr>
+				</tbody>
+			</table>
+		</div>
 
 		<div class="notification is-info">
 			<p class="title">Описание</p><br>
