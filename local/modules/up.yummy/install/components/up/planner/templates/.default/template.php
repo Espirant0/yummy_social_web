5a3fe5b0efb792f89a3f210c736d691d36982192
Mikhail Korol <mishakorol101@gmail.com>
2024-04-17 19:51:15 +0200

    Тесты планировщика

--- local/modules/up.yummy/install/components/up/planner/templates/.default/template.php
+++ local/modules/up.yummy/install/components/up/planner/templates/.default/template.php
@@ -4,7 +4,7 @@
 		<button class="button">Текущая неделя</button>
 		<button class="button">Следующая неделя</button>
 	</div>
-	<table class="table is-bordered is-hoverable planner_table">
+	<!--<table class="table is-bordered is-hoverable planner_table">
 		<thead>
 		<tr>
 			<th class="is-info"></th>
@@ -91,7 +91,60 @@
 			</td>
 		</tr>
 		</tbody>
-	</table>
+	</table>-->
+	<?php
+	// Определение текущей недели и текущего дня
+	$currentWeek = date('W');
+	$currentDay = date('N');
+
+	// Проверка, входит ли текущий день в текущую неделю
+	if ($currentDay < 1 || $currentDay > 7) {
+		// Обновление недели, если текущий день не входит в текущую неделю
+		$currentWeek = date('W');
+	}
+
+	// Функция для отображения календаря
+	function displayCalendar($week) {
+		// Получение даты начала и конца недели
+		$startDate = date('Y-m-d', strtotime($week . 'W1'));
+		$endDate = date('Y-m-d', strtotime($week . 'W7'));
+
+		// Ваш код для отображения календаря на указанную неделю
+		// Например, можно использовать цикл для вывода дней недели и их дат
+
+		echo "Календарь для недели $week:";
+		echo "<br>";
+		echo "Начало недели: $startDate";
+		echo "<br>";
+		echo "Конец недели: $endDate";
+	}
+
+	// Отображение календаря для текущей недели
+	/*displayCalendar($currentWeek);
+
+	// Переход к предыдущей неделе
+	$previousWeek = $currentWeek - 1;
+	displayCalendar($previousWeek);
+
+	// Переход к следующей неделе
+	$nextWeek = $currentWeek + 1;
+	displayCalendar($nextWeek);*/
+	$currentDate = date('Y-m-d');
+
+	// Определение дня недели для текущей даты (от 1 до 7, где 1 - понедельник, 7 - воскресенье)
+	$currentDayOfWeek = date('N', strtotime($currentDate));
+
+	// Вычисление даты начала недели
+	$startDate = date('Y-m-d', strtotime('-' . ($currentDayOfWeek - 1) . ' days', strtotime($currentDate)));
+
+	// Вычисление даты конца недели
+	$endDate = date('Y-m-d', strtotime('+' . (7 - $currentDayOfWeek) . ' days', strtotime($currentDate)));
+
+	// Вывод даты начала и конца недели
+	echo "Дата начала недели: $startDate";
+	echo "<br>";
+	echo "Дата конца недели: $endDate";
+	?>
 </div>
 
 <a href="#x" class="overlay" id="win1"></a>
