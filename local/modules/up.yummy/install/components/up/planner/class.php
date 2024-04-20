45301b7e36c8ce2d20454422ae59f5195cf9b4ce
Mikhail Korol <mishakorol101@gmail.com>
2024-04-20 19:24:15 +0200

    Даты в планировщике

--- local/modules/up.yummy/install/components/up/planner/class.php
+++ local/modules/up.yummy/install/components/up/planner/class.php
@@ -4,6 +4,39 @@ class PlannerComponent extends CBitrixComponent
 {
 	public function executeComponent()
 	{
+		$this->getDates();
 		$this->includeComponentTemplate();
+	}
+
+	protected function getDates(): void
+	{
+		$this->arResult['WEEK_DAYS'] = [
+			"Понедельник",
+			"Вторник",
+			"Среда",
+			"Четверг",
+			"Пятница",
+			"Суббота",
+			"Воскресенье",
+		];
+
+		$this->arResult['CURR_DATE'] = date('d.m.Y');
+
+		if (isset(request()['date']) && is_numeric(strtotime(request()['date'])))
+		{
+			$date = request()['date'];
+			$currentDayOfWeek = date('N', strtotime($date));
+		}
+		else
+		{
+			$date = $this->arResult['CURR_DATE'];
+			$currentDayOfWeek = date('N', strtotime($date));
+		}
+
+		$this->arResult['START_DATE'] = strtotime(date('d.m.Y', strtotime('-' . ($currentDayOfWeek - 1) . ' days', strtotime($date))));
+
+		$secondsInWeek = 604800;
+		$this->arResult['NEXT_WEEK'] = strtotime($date) + $secondsInWeek;
+		$this->arResult['PREV_WEEK'] = strtotime($date) - $secondsInWeek;
 	}
 }
--- local/modules/up.yummy/install/components/up/planner/templates/.default/template.php
+++ local/modules/up.yummy/install/components/up/planner/templates/.default/template.php
@@ -1,20 +1,22 @@
+<?php
+/**
+ * @var $arResult
+ */
+?>
 <div class="content">
 	<div class="buttons">
-		<button class="button">Предыдущая неделя</button>
-		<button class="button">Текущая неделя</button>
-		<button class="button">Следующая неделя</button>
+		<a href="/planner/?date=<?=date('d.m.Y', $arResult['PREV_WEEK'])?>" class="button">Предыдущая неделя</a>
+		<a href="/planner/" class="button">Текущая неделя</a>
+		<a href="/planner/?date=<?=date('d.m.Y',$arResult['NEXT_WEEK'])?>" class="button">Следующая неделя</a>
 	</div>
-	<!--<table class="table is-bordered is-hoverable planner_table">
+	<p class="has-text-centered">Сегодня <?=$arResult['CURR_DATE']?></p>
+	<table class="table is-bordered is-hoverable planner_table">
 		<thead>
 		<tr>
 			<th class="is-info"></th>
-			<th class="is-info">Понедельник <br> 08.04.2024</th>
-			<th class="is-info">Вторник <br> 09.04.2024</th>
-			<th class="is-info">Среда <br> 10.04.2024</th>
-			<th class="is-info">Четверг <br> 11.04.2024</th>
-			<th class="is-info">Пятница <br> 12.04.2024</th>
-			<th class="is-info">Суббота <br> 13.04.2024</th>
-			<th class="is-info">Воскресенье <br> 14.04.2024</th>
+			<?php for ($i = 0;$i < 7;$i++):?>
+				<th class="is-info is-primary"><?=$arResult['WEEK_DAYS'][$i]?> <br> <?=date("d.m.Y", strtotime('+'.$i.' day', $arResult['START_DATE']))?></th>
+			<?php endfor;?>
 		</tr>
 		</thead>
 		<tbody class="planner_table">
@@ -91,60 +93,7 @@
 			</td>
 		</tr>
 		</tbody>
-	</table>-->
-	<?php
-	// Определение текущей недели и текущего дня
-	$currentWeek = date('W');
-	$currentDay = date('N');
-
-	// Проверка, входит ли текущий день в текущую неделю
-	if ($currentDay < 1 || $currentDay > 7) {
-		// Обновление недели, если текущий день не входит в текущую неделю
-		$currentWeek = date('W');
-	}
-
-	// Функция для отображения календаря
-	function displayCalendar($week) {
-		// Получение даты начала и конца недели
-		$startDate = date('Y-m-d', strtotime($week . 'W1'));
-		$endDate = date('Y-m-d', strtotime($week . 'W7'));
-
-		// Ваш код для отображения календаря на указанную неделю
-		// Например, можно использовать цикл для вывода дней недели и их дат
-
-		echo "Календарь для недели $week:";
-		echo "<br>";
-		echo "Начало недели: $startDate";
-		echo "<br>";
-		echo "Конец недели: $endDate";
-	}
-
-	// Отображение календаря для текущей недели
-	/*displayCalendar($currentWeek);
-
-	// Переход к предыдущей неделе
-	$previousWeek = $currentWeek - 1;
-	displayCalendar($previousWeek);
-
-	// Переход к следующей неделе
-	$nextWeek = $currentWeek + 1;
-	displayCalendar($nextWeek);*/
-	$currentDate = date('Y-m-d');
-
-	// Определение дня недели для текущей даты (от 1 до 7, где 1 - понедельник, 7 - воскресенье)
-	$currentDayOfWeek = date('N', strtotime($currentDate));
-
-	// Вычисление даты начала недели
-	$startDate = date('Y-m-d', strtotime('-' . ($currentDayOfWeek - 1) . ' days', strtotime($currentDate)));
-
-	// Вычисление даты конца недели
-	$endDate = date('Y-m-d', strtotime('+' . (7 - $currentDayOfWeek) . ' days', strtotime($currentDate)));
-
-	// Вывод даты начала и конца недели
-	echo "Дата начала недели: $startDate";
-	echo "<br>";
-	echo "Дата конца недели: $endDate";
-	?>
+	</table>
 </div>
 
 <a href="#x" class="overlay" id="win1"></a>
